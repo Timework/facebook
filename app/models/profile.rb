2b595fccd37fd7bba5e72bca2acc96ce85902206
Christopher Martinez <timework.bibo@gmail.com>
2021-03-11 17:52:05 -0600

    changing to local to delete users

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :image, dependent: :destroy
   validates :name, presence: true
   validates :about, presence: true
 end
